be453906d6fba6243eb19b41b01b1efe5e1f86e7
sajad deaa <54747573+saladtosser@users.noreply.github.com>
2024-11-29 18:34:48 +0300

    Mailerinvite updated
    to work with the lastest smtp settings for namecheap on heroku dyno

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,7 +1,15 @@
 class RegistrationMailer < ApplicationMailer
+  default from: 'mail@iraqitechclub.com'  # Default sender email
+
   def invite_email(registration)
     @registration = registration
-    mail(to: @registration.email, subject: 'Session Invitation')
+    # Sending the email and rendering the view
+    mail(
+      to: @registration.email,
+      subject: 'Session Invitation'
+    ) do |format|
+      format.html { render 'registration_mailer/invite_email' }  # Use the correct view path
+      format.text { render 'registration_mailer/invite_email' }   # You can also include a text version if needed
+    end
   end
 end
-
