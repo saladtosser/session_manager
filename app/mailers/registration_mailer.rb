71ac361ceea0e861df9c7fccdd97c2cfbb4a0875
sajad deaa <54747573+saladtosser@users.noreply.github.com>
2024-11-29 18:43:05 +0300

    Another registerationmailer update
    Added logging for errors and stuff like that

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -3,6 +3,10 @@ class RegistrationMailer < ApplicationMailer
 
   def invite_email(registration)
     @registration = registration
+
+    # Log to check if the email method is being called
+    Rails.logger.info "Sending session invitation email to #{@registration.email}"
+
     # Sending the email and rendering the view
     mail(
       to: @registration.email,
@@ -11,5 +15,8 @@ class RegistrationMailer < ApplicationMailer
       format.html { render 'registration_mailer/invite_email' }  # Use the correct view path
       format.text { render 'registration_mailer/invite_email' }   # You can also include a text version if needed
     end
+
+    # Log after the email has been sent
+    Rails.logger.info "Email successfully sent to #{@registration.email}"
   end
 end
