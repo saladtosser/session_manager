43ce551db678115f9078373d93b4299fcea61432
sajad deaa <54747573+saladtosser@users.noreply.github.com>
2024-11-29 18:54:47 +0300

    Mailer fixes
    Here we fricking go

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,22 +1,14 @@
-class RegistrationMailer < ApplicationMailer
-  default from: 'mail@iraqitechclub.com'  # Default sender email
+def invite_email(registration)
+  @registration = registration
 
-  def invite_email(registration)
-    @registration = registration
+  Rails.logger.info "Sending session invitation email to #{@registration.email}"
 
-    # Log to check if the email method is being called
-    Rails.logger.info "Sending session invitation email to #{@registration.email}"
-
-    # Sending the email and rendering the view
-    mail(
-      to: @registration.email,
-      subject: 'Session Invitation'
-    ) do |format|
-      format.html { render 'registration_mailer/invite_email' }  # Use the correct view path
-      format.text { render 'registration_mailer/invite_email' }   # You can also include a text version if needed
-    end
-
-    # Log after the email has been sent
-    Rails.logger.info "Email successfully sent to #{@registration.email}"
+  mail(
+    to: @registration.email,
+    subject: 'Session Invitation'
+  ) do |format|
+    format.html { render 'registration_mailer/invite_email' }
   end
+
+  Rails.logger.info "Email successfully sent to #{@registration.email}"
 end
