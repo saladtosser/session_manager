9fcb75883aa1220e2c556cc5bd09b61799f98cd4
sajad deaa <54747573+saladtosser@users.noreply.github.com>
2024-12-14 13:21:23 +0300

    Some email updates
    blah blah blah

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -5,20 +5,20 @@ class RegistrationMailer < ApplicationMailer
     @registration = registration
 
     # Log for debugging
-    Rails.logger.info "Preparing to send session invitation email to #{@registration.email}"
+    Rails.logger.info "[Mailer] Preparing to send session invitation email to #{@registration.email}"
 
     # Construct and send the email
     mail(
       to: @registration.email,
       subject: 'ITC Session Invitation'
     ) do |format|
-      format.html { render 'registration_mailer/invite_email' }
+      format.html # This automatically looks for the `invite_email.html.erb` template
     end
 
     # Log success
-    Rails.logger.info "Invitation email sent successfully to #{@registration.email}"
+    Rails.logger.info "[Mailer] Invitation email sent successfully to #{@registration.email}"
   rescue StandardError => e
-    Rails.logger.error "Error sending email to #{@registration.email}: #{e.message}"
+    # Log error for better debugging
+    Rails.logger.error "[Mailer] Error sending email to #{@registration.email}: #{e.message}"
   end
 end
-
